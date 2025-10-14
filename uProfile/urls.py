c3797141293d1fd5e460220ab9e43364eca75e69
Shahin_luttu <shahinluttu@gamil.com>
2025-10-14 21:13:10 +0530

    feat: Added Edit, Delete, and Set Default Address functionalities

--- uProfile/urls.py
+++ uProfile/urls.py
@@ -8,14 +8,12 @@ urlpatterns = [
     path("change-password/", views.change_password, name="change_password"),
 
 
+    # Address Management
     path('profile/address/add/', views.add_address, name='add_address'),
-    path("address/set-default/<int:address_id>/", views.set_default_address, name="set_default_address"),
-
-    # Add these for future functionality
+    path('profile/address/delete/<int:address_id>/', views.delete_address, name='delete_address'),
     path('profile/address/edit/<int:address_id>/', views.edit_address, name='edit_address'),
-    # path('profile/address/delete/<int:address_id>/', views.delete_address, name='delete_address'),
-
-
+    path('profile/address/set-default/<int:address_id>/', views.set_default_address, name='set_default_address'),
+    
 
     path("orders/", views.order, name="order"),
     path("order_detail", views.order_detail, name='order_detail')
