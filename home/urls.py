c7e6d2995c2ee8f5fbf4ad3e99f4d1b58ec87f25
Shahin_luttu <shahinluttu@gamil.com>
2025-11-14 17:47:06 +0530

    feat: brand detail page

--- home/urls.py
+++ home/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('logout/', views.user_logout, name='logout'),
     path('products/', views.products, name='products'),
-    path("products/details/<int:product_id>/", views.product_detail, name="product_detail"),
+    path('products/details/<int:product_id>/', views.product_detail, name='product_detail'),
     path('wishlist/', views.wishlist, name='wishlist'),
+    path('brands/', views.brands, name='brands_list'),
 ]
