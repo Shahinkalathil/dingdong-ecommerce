052f52fd6479d31f3bf9bd4342050eb909a8f260
Shahin_luttu <shahinluttu@gamil.com>
2026-01-27 00:49:51 +0530

    feat: product edit is completed

--- offers/models.py
+++ offers/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
-from products.models import Brand
+from products.models import Brand, Product
 from django.utils import timezone
 
 
@@ -14,6 +14,16 @@ class BrandOffer(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
-
     def __str__(self):
         return f"{self.brand.name} - {self.discount_percentage}%"
+    
+class ProductOffer(models.Model):
+    product = models.OneToOneField(Product, on_delete=models.CASCADE, related_name='product_offer')
+    discount_percentage = models.DecimalField(max_digits=5, decimal_places=2, validators=[MinValueValidator(1), MaxValueValidator(100)],)
+    is_active = models.BooleanField(default=True)
+    valid_from = models.DateTimeField(default=timezone.now)
+    valid_until = models.DateTimeField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.product.name} - {self.discount_percentage}%"
