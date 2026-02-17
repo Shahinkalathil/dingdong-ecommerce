1a6d2c8d31c80b3ff7db01e793704f49d05d9a2d
Shahin_luttu <shahinluttu@gamil.com>
2026-02-18 02:00:26 +0530

    feat: create the dashboard line chart and download sale pdf

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
     path('', views.DashboardHomeView, name='admin_index'),
     path('sales-report/', views.sales_report_view, name='sales_report'),
-    path('sales-report/download-pdf/', views.download_sales_pdf, name='download_sales_pdf'),
+    path('sales-report/pdf/', views.download_sales_pdf, name='download_sales_pdf'),
 ]
+
 
